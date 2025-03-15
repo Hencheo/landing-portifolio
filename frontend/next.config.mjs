4537002ba375fa948fa6ad74789c5d940c9b68e0
Hencheo <hencheo96@gmail.com>
2025-03-14 22:01:48 -0300

    Configurando GitHub Pages

---
+++ frontend/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  basePath: '/landing-portifolio',
+  assetPrefix: '/landing-portifolio',
+};
+
+export default nextConfig; 
