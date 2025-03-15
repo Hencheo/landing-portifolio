e1f3f6b7c76dc1db61a1969489a327eb0f5e7d86
Hencheo <hencheo96@gmail.com>
2025-03-14 22:16:07 -0300

    Corrigindo configuração de recursos estáticos

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    domains: ['fonts.googleapis.com', 'fonts.gstatic.com'],
   },
   eslint: {
     ignoreDuringBuilds: true,
@@ -11,8 +12,22 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   basePath: '/landing-portifolio',
-  assetPrefix: '/landing-portifolio/',
-  trailingSlash: true
-};
+  assetPrefix: '',
+  trailingSlash: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]',
+          publicPath: '/landing-portifolio',
+        },
+      },
+    });
+    return config;
+  },
+}
 
 export default nextConfig; 
